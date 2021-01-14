b781a13cab95e7fd7a84916695ec78c6d0049168
Nikos Marmaridis <marmaridisn@gmail.com>
2021-01-15 01:15:18 +0200

    Update Author.vue

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -12,8 +12,8 @@
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+			<a href="//twitter.com/MummRa_GR">Follow on Twitter</a>
+			<a href="//github.com/marmanik">GitHub</a>
 		</p>
 
 	</div>
